eef0f0dac485e8d0096d111a0dca83104e06736c
Ryan Smith <0ryansmith1994@gmail.com>
2016-12-27 00:56:50 +0000

    Removes some duplicate code.

--- src/index.test.js
+++ src/index.test.js
@@ -4,6 +4,11 @@ const rulr = require('./index');
 
 const number = rulr.checkType(Number);
 
+const assertRule = (rule, data, expectedResult) => {
+  const actualResult = rule(data, ['data']);
+  assert.deepEqual(actualResult, expectedResult);
+};
+
 const lessThan10 = (data, path) =>
   data < 10 ? [] : [rulr.pathError(`${data} should be less than 10`)(path)];
 
@@ -51,63 +56,39 @@ describe('typeError', () => {
 
 describe('composeRules', () => {
   it('should return a new empty rule', () => {
-    const rules = [];
-    const data = 10;
-    const path = ['data'];
-    const actualResult = rulr.composeRules(rules)(data, path);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rulr.composeRules([]), 10, []);
   });
   it('should return a new rule', () => {
     const rules = [number, lessThan10];
-    const data = 'hello';
-    const path = ['data'];
-    const actualResult = rulr.composeRules(rules)(data, path);
-    const expectedResult = [
+    assertRule(rulr.composeRules(rules), 'hello', [
       '`\"hello\"` is not a valid Number in `data`',
-      'hello should be less than 10 in `data`'
-    ];
-    assert.deepEqual(actualResult, expectedResult);
+      'hello should be less than 10 in `data`',
+    ]);
   });
 });
 
 describe('first', () => {
+  const rule = rulr.first(number, lessThan10);
+
   it('should use the pre-requisite first', () => {
-    const data = 'hello';
-    const path = ['data'];
-    const actualResult = rulr.first(number, lessThan10)(data, path);
-    const expectedResult = [
-      '`\"hello\"` is not a valid Number in `data`'
-    ];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 'hello', ['`\"hello\"` is not a valid Number in `data`']);
   });
   it('should use the post-requisite second', () => {
-    const data = 10;
-    const path = ['data'];
-    const actualResult = rulr.first(number, lessThan10)(data, path);
-    const expectedResult = [
-      '10 should be less than 10 in `data`'
-    ];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 10, ['10 should be less than 10 in `data`']);
   });
 });
 
 describe('checkBool', () => {
   const isString = data => data.constructor === String;
   const error = data => rulr.pathError(`${data} is incorrect`);
-  const test = (data, expectedResult) => () => {
-    const actualResult = rulr.checkBool(isString, error)(data, ['data']);
-    assert.deepEqual(actualResult, expectedResult);
-  };
+  const rule = rulr.checkBool(isString, error);
 
-  it(
-    'should return an error if result is false',
-    test(10, ['10 is incorrect in `data`'])
-  );
-  it(
-    'should not return an error if result is true',
-    test('hello', [])
-  )
+  it('should return an error if result is false', () => {
+    assertRule(rule, 10, ['10 is incorrect in `data`']);
+  });
+  it('should not return an error if result is true', () => {
+    assertRule(rule, 'hello', []);
+  });
 });
 
 describe('checkThrow', () => {
@@ -117,106 +98,72 @@ describe('checkThrow', () => {
   const error = (data, ex) => rulr.pathError(`${data} error - ${ex.message}`);
 
   it('should return an exception message', () => {
-    const data = 10;
-    const actualResult = rulr.checkThrow(isString)(data, ['data']);
-    const expectedResult = ['10 is incorrect in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rulr.checkThrow(isString), 10, ['10 is incorrect in `data`']);
   });
   it('should return an error if an exception is thrown', () => {
-    const data = 10;
-    const actualResult = rulr.checkThrow(isString, error)(data, ['data']);
-    const expectedResult = ['10 error - 10 is incorrect in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    const rule = rulr.checkThrow(isString, error);
+    assertRule(rule, 10, ['10 error - 10 is incorrect in `data`']);
   });
   it('should not return an error if an exception is not thrown', () => {
-    const data = 'hello';
-    const actualResult = rulr.checkThrow(isString)(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rulr.checkThrow(isString), 'hello', []);
   });
 });
 
 describe('checkType', () => {
+  const rule = rulr.checkType(String, rulr.typeError);
+
   it('should return an error if the constructor is incorrect', () => {
-    const data = 10;
-    const actualResult = rulr.checkType(String, rulr.typeError)(data, ['data']);
-    const expectedResult = ['`10` is not a valid String in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 10, ['`10` is not a valid String in `data`']);
   });
   it('should not return an error if the constructor is correct', () => {
-    const data = 'hello';
-    const actualResult = rulr.checkType(String, rulr.typeError)(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 'hello', []);
   });
 });
 
 describe('checkRegex', () => {
   const pattern = /hello/;
   const error = data => rulr.pathError(`${data} is incorrect`);
-  const test = (data, expectedResult) => () => {
-    const actualResult = rulr.checkRegex(pattern, error)(data, ['data']);
-    assert.deepEqual(actualResult, expectedResult);
-  };
+  const rule = rulr.checkRegex(pattern, error);
 
-  it(
-    'should return an error if the data is not a string',
-    test(10, ['`10` is not a valid String in `data`'])
-  );
-  it(
-    'should return an error if the pattern is incorrect',
-    test('blabla', ['blabla is incorrect in `data`'])
-  );
-  it(
-    'should not return an error if the pattern is correct',
-    test('hello', [])
-  );
+  it('should return an error if the data is not a string', () => {
+    assertRule(rule, 10, ['`10` is not a valid String in `data`']);
+  });
+  it('should return an error if the pattern is incorrect', () => {
+    assertRule(rule, 'blabla', ['blabla is incorrect in `data`']);
+  });
+  it('should not return an error if the pattern is correct', () => {
+    assertRule(rule, 'hello', []);
+  });
 });
 
 describe('optional', () => {
-  const rule = rulr.checkType(String);
+  const postReq = rulr.checkType(String);
+  const rule = rulr.optional(postReq);
 
   it('should return an error if data is defined and incorrect', () => {
-    const data = 10;
-    const actualResult = rulr.optional(rule)(data, ['data']);
-    const expectedResult = ['`10` is not a valid String in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 10, ['`10` is not a valid String in `data`']);
   });
   it('should not return an error if data is undefined', () => {
-    const data = undefined;
-    const actualResult = rulr.optional(rule)(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, undefined, []);
   });
   it('should not return an error if data is defined and correct', () => {
-    const data = 'hello';
-    const actualResult = rulr.optional(rule)(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 'hello', []);
   });
 });
 
 describe('required', () => {
-  const rule = rulr.checkType(String);
+  const postReq = rulr.checkType(String);
   const error = rulr.missingKeyError;
+  const rule = rulr.required(postReq, error);
 
   it('should return an error if data is defined and incorrect', () => {
-    const data = 10;
-    const actualResult = rulr.required(rule, error)(data, ['data']);
-    const expectedResult = ['`10` is not a valid String in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 10, ['`10` is not a valid String in `data`']);
   });
   it('should return an error if data is undefined', () => {
-    const data = undefined;
-    const actualResult = rulr.required(rule, error)(data, ['data']);
-    const expectedResult = ['Missing required value in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, undefined, ['Missing required value in `data`']);
   });
   it('should not return an error if data is defined and correct', () => {
-    const data = 'hello';
-    const actualResult = rulr.required(rule, error)(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 'hello', []);
   });
 });
 
@@ -224,55 +171,39 @@ describe('restrictToSchema', () => {
   const schema = { foo: rulr.checkType(String) };
   const objectError = rulr.typeError;
   const keyError = rulr.invalidKeyError;
-  const validator = rulr.restrictToSchema(schema, objectError, keyError);
+  const rule = rulr.restrictToSchema(schema, objectError, keyError);
 
   it('should return an error data is not an object', () => {
-    const data = 10;
-    const actualResult = validator(data, ['data']);
-    const expectedResult = ['`10` is not a valid Object in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 10, ['`10` is not a valid Object in `data`']);
   });
   it('should return an error if keys are invalid', () => {
-    const data = { foo: 'hello', bar: 10 };
-    const actualResult = validator(data, ['data']);
     const expectedResult = ['Invalid keys `bar` found in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, { foo: 'hello', bar: 10 }, expectedResult);
   });
   it('should return an error if data is incorrect', () => {
-    const data = { foo: 10 };
-    const actualResult = validator(data, ['data']);
     const expectedResult = ['`10` is not a valid String in `data.foo`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, { foo: 10 }, expectedResult);
   });
   it('should not return an error if data is correct', () => {
     const data = { foo: 'hello' };
-    const actualResult = validator(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, { foo: 'hello' }, []);
   });
 });
 
 describe('restrictToCollection', () => {
-  const rule = index => rulr.checkType(String);
+  const postReq = index => rulr.checkType(String);
   const arrayError = rulr.typeError;
-  const validator = rulr.restrictToCollection(rule, arrayError);
+  const rule = rulr.restrictToCollection(postReq, arrayError);
 
   it('should return an error data is not an array', () => {
-    const data = 10;
-    const actualResult = validator(data, ['data']);
     const expectedResult = ['`10` is not a valid Array in `data`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, 10, expectedResult);
   });
   it('should return an error if data is incorrect', () => {
-    const data = [10];
-    const actualResult = validator(data, ['data']);
     const expectedResult = ['`10` is not a valid String in `data.0`'];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, [10], expectedResult);
   });
   it('should not return an error if data is correct', () => {
-    const data = ['hello'];
-    const actualResult = validator(data, ['data']);
-    const expectedResult = [];
-    assert.deepEqual(actualResult, expectedResult);
+    assertRule(rule, ['hello'], []);
   });
 });
