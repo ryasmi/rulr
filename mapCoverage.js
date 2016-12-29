010cde87af83b575ce7f81d81de90cf41f960547
Ryan Smith <0ryansmith1994@gmail.com>
2016-12-29 11:12:33 +0000

    Removes source map lcov.

--- mapCoverage.js
+++
@@ -1,8 +0,0 @@
-var lcovSourcemap = require('lcov-sourcemap');
-
-lcovSourcemap.writeLcov('./coverage/coverage.lcov', {
-  'index': './dist/index.js.map',
-  'index.test': './dist/index.test.js.map'
-}, './src', './coverage/coverage.lcov').then(function () {
-  console.log('done');
-});
