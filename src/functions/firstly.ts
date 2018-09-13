f6b5cd0486b9c728016f0d10bac89e91e93b0c0b
Ryan Smith <0ryansmith1994@gmail.com>
2018-09-13 13:43:41 +0100

    Temporary commit.

--- src/functions/firstly.ts
+++ src/functions/firstly.ts
@@ -1,15 +1,13 @@
-import ValidationError from '../errors/ValidationError';
 import Rule from '../Rule';
 
-export type Firstly = <V>(firstRule: Rule<V>, ...rules: Rule<any>[]) => Rule<V>;
+export type Firstly = <V1, V2>(rule1: Rule<V1>, rule2: Rule<V2>) => Rule<V1 & V2>;
 
-const firstly: Firstly = (...rules) => (data) => {
-  return rules.reduce((errors, rule) => {
-    if (errors.length === 0) {
-      return (rule as Rule<any>)(data);
-    }
-    return errors;
-  }, [] as ValidationError[]);
+const firstly: Firstly = (rule1, rule2) => (data) => {
+  const errorsOfRule1 = rule1(data);
+  if (errorsOfRule1.length === 0) {
+    return rule2(data);
+  }
+  return rule1(data);
 };
 
 export default firstly;
