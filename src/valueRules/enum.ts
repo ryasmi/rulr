f00c8cb176ae8c77d1bb041eb58d097bc9d26d9a
Ryan Smith <0ryansmith1994@gmail.com>
2020-05-23 10:28:17 +0100

    refactor: Puts error at the bottom

--- src/valueRules/enum.ts
+++ src/valueRules/enum.ts
@@ -1,13 +1,13 @@
-import { ValidationError } from '../errors/ValidationError';
+import { ValidationError } from '../errors/ValidationError'
 
-type Values<T> = T[keyof T];
+type Values<T> = T[keyof T]
 
 export function enumerated<Output>(enumerator: Output) {
-  return (input: unknown) => {
-    const enumValues = Object.values(enumerator);
-    if (enumValues.includes(input) === false) {
-      throw new ValidationError(`${input} not found in Enum`, input);
-    }
-    return input as any as Values<Output>;
-  };
+	return (input: unknown) => {
+		const enumValues = Object.values(enumerator)
+		if (enumValues.includes(input)) {
+			return (input as any) as Values<Output>
+		}
+		throw new ValidationError(`${input} not found in Enum`, input)
+	}
 }
